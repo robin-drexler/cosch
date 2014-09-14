97e1ab880407a0e015d107bf055f96eabfcec7a7
Robin Drexler <drexler.robin@gmail.com>
2014-09-14 19:53:49 +0200

    try to fix deploy script

--- lib/scripts/deploy_to_github_pages.sh
+++ lib/scripts/deploy_to_github_pages.sh
@@ -3,12 +3,21 @@ echo "Going to deploy the joy. Buckle up."
 
 GIT_REMOTE_URL=$1
 
+# thanks http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 if [ -z "$GIT_REMOTE_URL" ]; then
   echo "Missing git remote url. Aborting"
   exit 1
 fi
 
-ruby bin/build.rb build
+
+# xxx there must be a better way
+# why isn't even possible to call the script with ../../ in the first place?
+ABSOLUTE_SCRIPT_PATH=$(cd "$DIR/../../bin/" && pwd)
+
+"$ABSOLUTE_SCRIPT_PATH/build.rb" build
+
 cd build
 rm -rf .git
 git init
